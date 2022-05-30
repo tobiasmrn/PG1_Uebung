edf9ad5d75e6cd2076e924ff3fb9046691b3e63e
tobiasmrn <t.mohrmann@outlook.de>
2022-05-30 17:27:20 +0200

    auswahl

--- Übung10/personal.c
+++ Übung10/personal.c
@@ -9,6 +9,33 @@ typedef struct{
 
 
 int main (){
-    personenkartei p1;
+    int auswahl;
+    personenkartei *folder;
+    auswahl = get_auswahl("Was wollen Sie machen?\n1. Neue Person anlegen\n2. Personenliste ausgeben\n3. Person löschen\n4. Programm beenden\n", &auswahl);
+
     return 0;
+}
+
+int auswahl (char* question){
+    int auswahl;
+    printf("%s", question);
+    scanf("%d", &auswahl);
+    switch(auswahl){
+        case 1:
+            printf("Sie haben gewaehlt, dass Sie eine neue Person anlegen wollen.\n");
+            break;
+        case 2:
+            printf("Sie haben gewaehlt, dass Sie alle Personen ausgeben wollen.\n");
+            break;
+        case 3:
+            printf("Sie haben gewaehlt, dass Sie eine Person loeschen wollen.\n");
+            break;
+        case 4:
+            printf("Sie haben gewaehlt, dass Sie das Programm beenden wollen.\n");
+            break;
+        default:
+            printf("Sie haben eine ungueltige Eingabe gemacht.\n");
+            break;
+    }
+    return auswahl;
 }
