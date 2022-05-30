9f820eddb1300540bf305c1fd9da1ec04a59f8da
tobiasmrn <t.mohrmann@outlook.de>
2022-05-30 16:36:48 +0200

    Übung 10 Anfang

---
+++ Übung10/personal.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+typedef struct{
+    char name[50];
+    float gehalt;
+    int personalnummer;
+}personenkartei;
+
+
+
+int main (){
+    personenkartei p1;
+    return 0;
+}
