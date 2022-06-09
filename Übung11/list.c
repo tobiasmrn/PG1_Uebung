9a526bda1032a97aeb273d89ae4caa9881677ac9
tobiasmrn <t.mohrmann@outlook.de>
2022-06-09 16:25:44 +0200

    Schöhnheits Fixes

--- Übung11/list.c
+++ Übung11/list.c
@@ -10,57 +10,10 @@ typedef struct person{
     struct person* next;
 }person;
 
-int get_auswahl (char* question){
-    int auswahl;
-    printf("%s", question);
-    scanf("%d", &auswahl);
-    return auswahl;
-}
-
-void person_anlegen(person* tail){
-    person* new_person = (person*)malloc(sizeof(person));
-    printf("\nBitte geben Sie den Namen ein: ");
-    scanf("%50s", &new_person->name);
-    printf("Bitte geben Sie das Gehalt ein: ");
-    scanf("%f", &new_person->gehalt);
-    printf("Bitte geben Sie die Personalnummer ein: ");
-    scanf("%d", &new_person->personalnummer);
-    new_person->prev = tail->prev;
-    new_person->next = tail;
-    tail->prev->next = new_person;
-    tail->prev = new_person;
-    printf("\nPerson angelegt!\n");
-                
-}
-
-void print_all(person* head){
-    person* current = head;
-    if(current->prev == NULL){
-        current = current->next;
-    }
-    while(current->next != NULL && current->prev != NULL){
-        printf("\nName: %s\n", current->name);
-        printf("Gehalt: %.2f\n", current->gehalt);
-        printf("Personalnummer: %d\n", current->personalnummer);
-        current = current->next;
-    }
-}
-
-void delete_person(person* head){
-    int personalnummer;
-    printf("\nBitte geben Sie die Personalnummer ein: ");
-    scanf("%d", &personalnummer);
-    person* current = head;
-        while(current != NULL){
-            if(current->personalnummer == personalnummer){
-                current->prev->next = current->next;
-                current->next->prev = current->prev;
-                free(current);
-            }
-            current = current->next;
-        }
-    printf("\nPerson gelöscht!\n");
-}
+int get_auswahl (char* question);
+void person_anlegen(person* tail);
+void print_all(person* head);
+void delete_person(person* head);
 
 int main(){
     person anker1;
@@ -100,3 +53,57 @@ int main(){
     }
     return 0;
 }
+
+int get_auswahl (char* question){
+    int auswahl;
+    printf("%s", question);
+    scanf("%d", &auswahl);
+    return auswahl;
+}
+
+void person_anlegen(person* tail){
+    person* new_person = (person*)malloc(sizeof(person));
+    printf("\nBitte geben Sie den Namen ein: ");
+    scanf("%50s", &new_person->name);
+    printf("Bitte geben Sie das Gehalt ein: ");
+    scanf("%f", &new_person->gehalt);
+    printf("Bitte geben Sie die Personalnummer ein: ");
+    scanf("%d", &new_person->personalnummer);
+    new_person->prev = tail->prev;
+    new_person->next = tail;
+    tail->prev->next = new_person;
+    tail->prev = new_person;
+    printf("\nPerson angelegt!\n");
+                
+}
+
+void print_all(person* head){
+    person* current = head;
+    if(current->prev == NULL){
+        current = current->next;
+    }
+    while(current->next != NULL && current->prev != NULL){
+        printf("\nName: %s\n", current->name);
+        printf("Gehalt: %.2f Euro\n", current->gehalt);
+        printf("Personalnummer: %d\n", current->personalnummer);
+        current = current->next;
+    }
+}
+
+void delete_person(person* head){
+    int personalnummer;
+    printf("\nBitte geben Sie die Personalnummer ein: ");
+    scanf("%d", &personalnummer);
+    person* current = head;
+        while(current != NULL){
+            if(current->personalnummer == personalnummer){
+                current->prev->next = current->next;
+                current->next->prev = current->prev;
+                free(current);
+            }
+            current = current->next;
+        }
+    printf("\nPerson gelöscht!\n");
+}
+
+
